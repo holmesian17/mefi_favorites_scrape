f898183373be72b58fffc58ffbf3e88dc5f32d85
Ian <holmesian17@gmail.com>
2021-04-25 15:23:03 -0600

    Update mefi_scraper.py

--- mefi_scraper.py
+++ mefi_scraper.py
@@ -1,3 +1,10 @@
-import stuff
+import bs4
+import requests
 
 url = "https://www.metafilter.com/favorites/169597/comments/ask/" #must be logged into mefi to access this page
+
+res = requests.get(url)
+res.raise_for_status()
+
+soup = bs4.BeautifulSoup(res.text, 'html.parser')
+
